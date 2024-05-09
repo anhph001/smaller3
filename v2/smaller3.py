dea67c4e9661840babd2c75cf194125fdca5697d
100014@ispschools.edu.vn <121950392+AlexPham6000@users.noreply.github.com>
2024-05-09 07:39:28 +0700

    updated

--- v2/smaller3.py
+++ v2/smaller3.py
@@ -3,8 +3,32 @@ from flask import Flask, render_template, session
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return 'main route'
+def landing():
+    return render_template('landing.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html', username="anhpham")
+
+@app.route('/smaller3')
+def smaller3():
+    return 'smaller3'
+
+@app.route('/faq')
+def faq():
+    return 'faq'
+
+@app.route('/zodiac')
+def zodiac():
+    return 'zodiac'
+
+@app.route('/message3')
+def message3():
+    return 'message3 - under developement'
+
+@app.route('/news')
+def news():
+    return 'news - under development'
 
 if __name__ == "__main__":
     app.run(debug=True)
