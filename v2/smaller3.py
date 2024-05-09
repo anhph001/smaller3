98ea9363b7ca16d43c32ee9f0c21f444801e859d
100014@ispschools.edu.vn <121950392+AlexPham6000@users.noreply.github.com>
2024-05-09 07:52:16 +0700

    later updated

--- v2/smaller3.py
+++ v2/smaller3.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, session
 
 app = Flask(__name__)
 
+info = {"first_person_name": "", "second_person_name": ""}
+
 @app.route('/')
 def landing():
     return render_template('landing.html')
@@ -12,7 +14,11 @@ def home():
 
 @app.route('/smaller3')
 def smaller3():
-    return 'smaller3'
+    return render_template('smaller3.html',
+                           first_person_name=info["first_person_name"],
+                           second_person_name=info['second_person_name'],
+                           first_person_image='img/fpi.jpg'
+                           )
 
 @app.route('/faq')
 def faq():
